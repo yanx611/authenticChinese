4d51128f56a25011bb307d150a8c1283ba8f0c9a
Etsu <yanx611@gmail.com>
2019-02-20 00:29:21 -0500

    structure of homepage

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {Route, Link, BrowserRouter as Router, Switch} from 'react-router-dom';
 import App from './App';
+import Notfound from './Notfound';
 import * as serviceWorker from './serviceWorker';
 import * as firebase from 'firebase';
 
@@ -17,7 +19,16 @@ messagingSenderId: "337704036974"
 firebase.initializeApp(config);
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+    <Router>
+        <Switch>
+            <Route exact path = "/" component={App} />
+            <Route component={Notfound} />
+        </Switch>
+    </Router>
+)
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
