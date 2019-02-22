aa995a4c71d255c8c97555b167f6c4abf608e816
Etsu <yanx611@gmail.com>
2019-02-21 21:01:15 -0500

    notes about firebase and set up configurations

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import firebase from './Firebase';
 import Home from './Home';
 import FooterInfo from './FooterInfo';
 import MenuEntry from './MenuEntry';
@@ -9,6 +10,26 @@ import 'antd/dist/antd.css';
 const { Header, Content, Footer } = Layout;
 
 class App extends Component {
+	componentDidMount() {
+		// const datRef = firebase.database().ref('clips');
+		const db = firebase.firestore();
+		// db.collection("clips").where("chineseName", "==", "小爸爸")
+		// .get()
+		// .then(snap=>{
+		// 	snap.forEach(doc=>{
+		// 		console.log(doc.data());
+		// 	})
+		// });
+
+		// if without constriants
+		db.collection("clips")
+		.get()
+		.then(snap=>{
+			snap.forEach(doc=>{
+				console.log(doc.data());
+			})
+		});
+	}
 	render() {
 		return (
 			<div className="App">
